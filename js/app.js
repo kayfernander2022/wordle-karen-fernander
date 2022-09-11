1fe033a26ed239a5b4c72faf04147ff64908f4a3
kayfernander2022 <kayfernander@hotmail.com>
2022-09-10 21:20:15 -0400

    initial board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,100 @@
-console.log('sanity')
+/*-------------------------------- Constants --------------------------------*/
+const gameTheme =[]
+const keys =[]
+
+
+
+//)    Define the required variables used to track the state of the game 
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+/
+init();
+
+function init() {
+}
+
+function playerGuess (){
+  
+}
+
+//Board variable that is an array to keep track of words entered and rows still available to player. 
+
+//Keyboard array variable to keep track of letters and set state based on not used yet (white), correct place in word (Green), part of word but wrong place (Orange), not correct (Dark grey) 
+
+//Theme variable to hold the selected theme. 
+//Word to guess variable to hold the random word that the user needs to find. 
+
+
+
+//Store cached element references 
+
+//Variable to store game messages, keyboard keys, board (rows and columns), reset button, theme selection elements. 
+
+
+//Upon loading, the game state should be initialized, and a function should be  
+
+//called to render this game state 
+
+//Init function to setup the game 
+
+//Generate Theme selection 
+
+//render board and visual keyboard 
+
+//Select random word from word bank 
+
+//Render message to user that they can begin the game. 
+
+//Define the required constants 
+
+//Array of available themes 
+//The messages that can be displayed to the user 
+
+//Handle a player clicking a tile with a `handleClick` function 
+
+//Move focus to the row of the tile the user selects.   
+//Allow user to enter word starting at beginning of the row selected if word does not already exist. 
+//Add event listener to each key, button 
+
+//Function to lock rows with words already entered 
+
+
+//Keyboard ‘handleClick’ function 
+
+//Change color of the keyboard letter tiles based on words being entered 
+
+//Function to return list of themes to display to player 
+
+
+//Function to check if word entered by user is correct 
+
+//Identify if whole word is correct 
+//If not determine if letter exist that is in correct position 
+//Else determine if letter exist in word but in wrong position 
+//Else letter is wrong. 
+
+//Render a message based on the current game state 
+
+//render a message that the player has won or loss 
+//render a message showing the number of tries the user has left 
+
+//Create Reset functionality 
+
+//Call init to reset the board when reset button is clicked 
+
+//Add a responsive design 
+
+//Add a favicon 
+
+//Add google fonts 
+
